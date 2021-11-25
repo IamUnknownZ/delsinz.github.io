24014dfcd67e3cc6e7119db8b9c83b3e029ba0f2
Delsinz <95037444+IamUnknownZ@users.noreply.github.com>
2021-11-25 23:25:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,20 +27,7 @@ window.onload = function(){
         audio.play();
     });
 
-document.body.onkeydown = function(e){
-    if(e.keyCode ==32){
-        increaseScore();
-        img.src = 'popcat2.png';
-        audio.play();
-    }
-}
 
-document.body.onkeyup = function(e){
-    if(e.keyCode ==32){
-        img.src = 'popcat1.png';
-        audio.play();
-    }
-}
     function increaseScore(){
         score++;
         count.innerHTML = score;
